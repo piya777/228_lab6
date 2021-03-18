7ad4237b2556256335e7256d4c13fd3c182ac079
Piya <piya.rat@ku.th>
2021-03-18 16:12:00 +0700

    4

--- 6320503068_6_3.c
+++ 6320503068_6_3.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
 
     void main(){
-
+        int n,q,i,o,t,max=0;
+        scanf("%d %d",&n ,&q);
+        int x[n+1];
+        for( i=0 ; i<=n ; i++ ){
+            x[i]=0;
+        }
+        char fuct;
+        for( i=0 ; i<=q ; i++ ){
+            fflush(stdin);
+            scanf("%c %d %d",&fuct,&o,&t);
+            if(fuct=='U'){
+               x[o]=t;
+            }
+            else if(fuct=='P'){
+                for( i=o ; i<=t ; i++ ){
+                    if(max<x[i]){
+                        max=x[i];
+                    }
+                }
+                printf("%d\n",max);
+            }
+        }
     }
--- 6320503068_6_5.c
+++ 6320503068_6_5.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
     void main(){
-
+        int w,h;
+        scanf("%d %d",&w ,&h);
+        int x[w][h]
+        for( i=0 ; i<h ; i++ ){
+            for( j=0 ; j<w ; j++ ){
+                scanf("%d",&x[i][j]);
+            }
+        }
     }
