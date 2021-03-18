5900b29242f27b3ae6faa6d659a4a4d7a2bbeed9
Piya <piya.rat@ku.th>
2021-03-18 18:07:54 +0700

    8

--- 6320503068_6_4.c
+++ 6320503068_6_4.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 
     void main(){
+        int n,i,p[1000],max=0,min=0,x,y,m;
+        scanf("%d",&n);
+        for( i=0 ; i<n ; i++ ){
+            scanf("%d %d",&x,&y);
+            p[x-1]++;
+        }
+        for( i=0 ; i<n ; i++ ){
+            if(max<p[i]){
+                max=i;
+            }
+        }
+        min=max;
+        for( i=0 ; i<n ; i++ ){
+            if(min>p[i]){
+                min=i;
+            }
+        }
+        printf("%d %d",max,min);
 
     }
