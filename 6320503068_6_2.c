545596d63eac7e5d28d706b425b6b804508b3493
Piya <piya.rat@ku.th>
2021-03-18 15:21:07 +0700

    4

--- 6320503068_6_2.c
+++ 6320503068_6_2.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+#include<string.h>
 
     void main(){
-
+        int n,x=0,i;
+        scanf("%d",&n);
+        char real[n],tho[n];
+        scanf("%s",real);
+        scanf("%s",tho);
+        for( i=0 ; i<n ; i++ ){
+            if(real[i]==tho[i]){
+                x++;
+            }
+        }
+        printf("%d %d",x,n-x);
     }
