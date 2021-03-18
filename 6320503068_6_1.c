52ca9d8b02f541536fbe8c7a18fc3bfade39fabc
Piya <piya.rat@ku.th>
2021-03-18 15:09:32 +0700

    3

--- 6320503068_6_1.c
+++ 6320503068_6_1.c
@@ -2,22 +2,24 @@
 #include<string.h>
 
     void main(){
-        int n,i,j,k=0;
+        int n,i,j,k;
         scanf("%d",&n);
         char word[n][1000],token[1000];
         for( i=0 ; i<n ; i++ ){
             scanf("%s",word[i]);
         }
         for( i=0 ; i<n ; i++ ){
-            for( j=0 ; j<n-i-1 ; j++ ){
-                    if(word[j][k]>word[j+1][k]){
-                        strcpy(token,word[j+1]);
-                        strcpy(word[j+1],word[j]);
-                        strcpy(word[j],token);
-                    }
+            for( j=i+1 ; j<n ; j++ ){
+                if(strcmp(word[i],word[j])>0){
+                    strcpy(token,word[j]);
+                    strcpy(word[j],word[i]);
+                    strcpy(word[i],token);
                 }
             }
-           // printf("%s %s %s %s %s\n",word[0],word[1],word[2],word[3],word[4]);
+           // printf("%s %s %s %s %s\n",word[0],word[1],word[2],word[3]
         }
-
+        for( i=0 ; i<n ; i++ ){
+            printf("%s\n",word[i]);
+        }
     }
+
