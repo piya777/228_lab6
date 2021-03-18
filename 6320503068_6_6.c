a4a057bae9e444dee8345589be4ab3aed6ff14e0
Piya <piya.rat@ku.th>
2021-03-18 17:50:43 +0700

    7

--- 6320503068_6_6.c
+++ 6320503068_6_6.c
@@ -12,6 +12,72 @@
                 paper[i][j] = '.';
             }
         }
+        for( i=0 ; i<strlen(order) ; i++ ){
+            if(order[i]=='U'||order[i]=='D'){
+                if(x>0&&order[i] == 'U'){
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='|';
+                    }
+                    else if(paper[x][y]=='-'){
+                        paper[x][y]='+';
+                    }
+                    x--;
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='|';
+                    }
+                    else if(paper[x][y]=='-'){
+                        paper[x][y]='+';
+                    }
+                }
+                if(x<psize-1&&order[i] == 'D'){
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='|';
+                    }
+                    else if(paper[x][y]=='-'){
+                        paper[x][y]='+';
+                    }
+                    x++;
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='|';
+                    }
+                    else if(paper[x][y]=='-'){
+                        paper[x][y]='+';
+                    }
+                }
+            }
+            else  if(order[i]=='R' || order[i]=='L'){
+                if(y<psize-1&&order[i] == 'R'){
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='-';
+                    }
+                    else   if(paper[x][y]=='|'){
+                        paper[x][y]='+';
+                    }
+                    y++;
+                    if(paper[x][y]=='.'){
+                        paper[x][y]='-';
+                    }
+                    else if(paper[x][y]=='|'){
+                        paper[x][y]='+';
+                    }
+                }
+                if(y>0&&order[i] == 'L'){
+                    if( paper[x][y]=='.'){
+                        paper[x][y]='-';
+                    }
+                    else  if( paper[x][y]=='|'){
+                        paper[x][y]='+';
+                    }
+                    y--;
+                    if( paper[x][y]=='.'){
+                        paper[x][y]='-';
+                    }
+                    else if( paper[x][y]=='|'){
+                        paper[x][y]='+';
+                    }
+                }
+            }
+        }
         for( i=0 ; i<psize ; i++){
             for( j=0 ; j<psize ; j++){
                 printf("%c",paper[i][j]);
