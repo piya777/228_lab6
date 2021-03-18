9323188aa55f372e379c4f7cbf977c654137d5bd
Piya <piya.rat@ku.th>
2021-03-18 17:42:19 +0700

    6

--- 6320503068_6_6.c
+++ 6320503068_6_6.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 
-    void main(){
-
+    int main(){
+        int psize,i,j,x=0,y=0;
+        char order[250];
+        scanf("%d",&psize);
+        scanf("%s",order);
+        char paper[psize][psize];
+        for( i=0; i<psize ; i++ ){
+            for( j=0; j<psize; j++ ){
+                paper[i][j] = '.';
+            }
+        }
+        for( i=0 ; i<psize ; i++){
+            for( j=0 ; j<psize ; j++){
+                printf("%c",paper[i][j]);
+            }
+            printf("\n");
+        }
     }
